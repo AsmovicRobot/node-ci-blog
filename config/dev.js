66c7287b5d4d388a37d08db8bce67b0caba947ba
AsmovicRobot <asogbatawa@gmail.com>
2021-07-23 12:38:26 +0100

    Add image upload

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'YPc3Q0oDk6K_apsVk4Vee7DT',
   mongoURI: "mongodb://asmovic:asmovic007@cluster0-shard-00-00.nw8fv.mongodb.net:27017,cluster0-shard-00-01.nw8fv.mongodb.net:27017,cluster0-shard-00-02.nw8fv.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-zi1iyt-shard-0&authSource=admin&retryWrites=true&w=majority",
   cookieKey: '123123123',
-  redisURL: 'redis://127.0.0.1:6379'
+  redisURL: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAVIRWWP2PYRAARRAG',
+  secretAccessKey: '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'
 };
